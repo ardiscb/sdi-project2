4a0d4ca5c63e665df4fb45b972c0397161016612
ardiscb <ardis.courtney@gmail.com>
2012-05-10 17:25:25 -0300

    Added a procedure and string function as well as a stub array function and comments.

--- ardis_courtney_assignment_2.js
+++ ardis_courtney_assignment_2.js
@@ -5,13 +5,25 @@ Assignment: Deliverable 2
 Term: 1205
 */
 
-var procedure = function (argument){
-	if (argument = 0) {
-		console.log("");
+//This is not a part of this project. This function is used to abort the script for the procedure conditional.
+var javascriptAbort = function () {
+   		throw new Error('This is NOT an error. This is used to abort the script.');
+};
+//END javascriptAbort
+
+//START
+
+//Procedure
+var ticketStatus = function (ticket){
+	if (ticket === true) {
+		console.log("Your ticket has been created.");
 	} else {
-		console.log("");
+		console.log("Your ticket has not been created yet. Please call us so we can get your problem solved");
+		javascriptAbort();
+
 	}
-}
+};
+//END ticketStatus
 
 //Boolean function
 //Determines the team that has been assigned to a ticket 
@@ -27,27 +39,61 @@ var determineTeam = function (hardwareTeamIssue, programmingTeamIssue) {
 		team = console.log("A technician from the help desk team has been assigned to your ticket.");
 		return team;
 	}
-}
+};
+//END determineTeam
 
 //Number function
-//Reminder to do call backs
-//Need to change logic/math
-var voicemailReminder = function (voicemails) {
-	var voicemails = i;
-	var i = 1;
-	while(i < 10) {
-		console.log("You have " + i + " to call back.");
-		i++;
+//Remindes team every 15 minutes to do call backs within hour of voicemail
+//Fixed infinite loop, still nothing displays
+var callBackReminder = function (minutesPassed) {
+	var minutesLeft = 60;
+	var minutesPassed = 0;
+	while(minutesPassed < 60 && minutesPassed > 0) {
+		console.log("Your team has " + minutesLeft + " minutes to do call backs.");
+		minutesPassed = minutesLeft - 15;
+	};
+	return minutesPassed;
+};
+//END callBackReminder
+
+//String function
+var nameTechnician = function(fname, lname) {
+	//local variables
+	var name = fname + " " + lname;
+	//string concatenation
+	console.log("Your technician's name is " + name);
+	return name;
+};
+//END nameTechnician
+
+//Array function
+var doArray = function(number, array) {
+	//local variables
+	for (i = 0; i > 10; i++) {
+		//Math
+		console.log("");
 	}
-	return voicemails;
-}
+	return array;
+};
+//END doArray
+
+//Outputs
 
 //Procedure call
-//procedure(argument);
+ticketStatus(true);
 
 //Boolean function call
 determineTeam(false, true);
 
-//Number function call
-voicemailReminder(4);
+console.log("All technicans must do call backs within an hour of the initial call.")
 
+//Number function call
+callBackReminder(20);
+
+//String function call
+nameTechnician("Courtney", "Ardis")
+
+//Array function call
+//doArray(5, array);
+
+//FINISH
